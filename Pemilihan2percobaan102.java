024cd4a1fa6fc3a6928100da80c9b4bc54642565
aidatulrosida <144088067+aidatulrosida@users.noreply.github.com>
2023-10-03 14:19:01 +0700

    Add files via upload

---
+++ Pemilihan2percobaan102.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pemilihan2percobaan102 {
+    public static void main(String[] args) {
+        Scanner input07= new Scanner (System.in);
+        int tahun;
+       
+        System.out.println("Masukkan Tahun :");
+        tahun =input07.nextInt();
+
+        if ((tahun % 4 == 0 && tahun % 100 !=0) || tahun % 400 == 0){
+            if ((tahun % 100) != 0 )
+                System.out.println("Tahun Kabisat");
+        }else{
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+    
+}
